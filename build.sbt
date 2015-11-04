f8466e7eae0585b079188dad8fad4a3b2cd63f22
Przemek Dyk <przemyslaw.dyk@smtsoftware.com>
2015-11-04 16:14:31 +0100

    simple use cases

--- build.sbt
+++ build.sbt
@@ -2,11 +2,8 @@ name := """avro-sandbox"""
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.10.5"
 
-// Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-
-// Uncomment to use Akka
-//libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
+libraryDependencies += "com.gensler" %% "scalavro" % "0.6.2"
 
---
+++ src/test/scala/sandbox/avro/AvroSpec.scala
@@ -0,0 +1,72 @@
+package sandbox.avro
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
+import com.gensler.scalavro.io.AvroTypeIO
+import com.gensler.scalavro.types.AvroType
+import org.scalatest._
+
+import scala.util.Success
+
+
+case class P(a: String, b: Int)
+case class R(a: String, b: Int)
+case class Q(a: String, renamed: Int)
+case class H(a: String, b: String)
+case class D(a: String)
+
+sealed trait Color
+
+object Color {
+  case object Red extends Color
+  case object Blue extends Color
+  case object Green extends Color
+}
+
+case class C(a: String, color: Color)
+
+
+class AvroSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+
+  val p = P("henio",12)
+  val serializedP = {
+    val out = new ByteArrayOutputStream()
+    val io: AvroTypeIO[P] = AvroType[P].io
+    io.write(p,out)
+    out.toByteArray
+  }
+
+  it should "handle ranamed case class" in {
+    val io: AvroTypeIO[R] = AvroType[R].io
+    val Success(r) = io.read(new ByteArrayInputStream(serializedP))
+    (r.a,r.b) should be (p.a,p.b)
+  }
+
+  it should "deserialize renamed field" in {
+    val io: AvroTypeIO[Q] = AvroType[Q].io
+    val Success(q) = io.read(new ByteArrayInputStream(serializedP))
+    (q.a, q.renamed) should be (p.a,p.b)
+  }
+
+  it should "deserialize ignoring missing field" in {
+    val io: AvroTypeIO[D] = AvroType[D].io
+    val Success(d) = io.read(new ByteArrayInputStream(serializedP))
+    d.a should be (p.a)
+  }
+
+  ignore should "deserialize field with changed type" in {
+    val io: AvroTypeIO[H] = AvroType[H].io
+    val Success(h) = io.read(new ByteArrayInputStream(serializedP))
+    h.b should be (p.b.toString)
+  }
+
+  ignore should "serialize enum" in {
+    println(AvroType[C].schema)
+    val io: AvroTypeIO[C] = AvroType[C].io
+    val out = new ByteArrayOutputStream()
+    io.write(C("a",Color.Red),out)
+    val Success(c) = io.read(new ByteArrayInputStream(out.toByteArray))
+    c.color should be (Color.Red)
+  }
+
+}
