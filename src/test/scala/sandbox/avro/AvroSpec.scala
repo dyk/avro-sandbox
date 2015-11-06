3db077512dc4984df7ce9a1e54937a27316f4014
Przemek Dyk <przemyslaw.dyk@smtsoftware.com>
2015-11-06 15:23:16 +0100

    example of java api

--- src/test/scala/sandbox/avro/AvroSpec.scala
+++ src/test/scala/sandbox/avro/AvroSpec.scala
@@ -4,102 +4,63 @@ import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
 import com.gensler.scalavro.io.AvroTypeIO
 import com.gensler.scalavro.types.AvroType
+import org.apache.avro.Schema
+import org.apache.avro.file.{SeekableByteArrayInput, DataFileReader, DataFileWriter}
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord, GenericDatumWriter, GenericData}
 import org.scalatest._
 
 import scala.util.Success
 
 
-case class P(a: String, b: Int)
-case class R(a: String, b: Int)
-case class Q(a: String, renamed: Int)
-case class H(a: String, b: String)
-case class G(a: String, b: Long)
-case class D(a: String)
-
-sealed trait Color
-
-object Color {
-  case object Red extends Color
-  case object Blue extends Color
-  case object Green extends Color
-}
-
-case class C(a: String, color: Color)
-
-
-object Currency extends Enumeration {
-  val GBP = Value("GBP")
-  val EUR = Value("EUR")
-  val USD = Value("USD")
-}
-
-case class E(a: String, currency: Currency.Value)
-
 class AvroSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
 
-  val p = P("henio",12)
-  val serializedP = {
-    val out = new ByteArrayOutputStream()
-    val io: AvroTypeIO[P] = AvroType[P].io
-    io.write(p,out)
-    out.toByteArray
-  }
+
+  val writeSchema = new Schema.Parser().parse(
+    """
+      |{"name":"P",
+      | "namespace":"",
+      | "type":"record",
+      | "fields":[
+      |     {"name":"a",
+      |     "type":"string"},
+      |     {"name":"b",
+      |     "type":"int"}]}
+    """.stripMargin)
+
+  val readSchema = new Schema.Parser().parse(
+    """
+      |{"name":"P",
+      | "namespace":"",
+      | "type":"record",
+      | "fields":[
+      |     {"name":"a",
+      |     "type":"string"},
+      |     {"name":"b",
+      |     "type":"float"}]}
+    """.stripMargin)
+
+  it should "serialize and deserialize" in {
+
+    val p = new GenericData.Record(writeSchema)
+    p.put("a", "hejka")
+    p.put("b", 12)
+
+    val datumWriter = new GenericDatumWriter[GenericRecord](writeSchema)
+    val dataFileWriter = new DataFileWriter[GenericRecord](datumWriter)
+    val data = new ByteArrayOutputStream()
+    dataFileWriter.create(writeSchema, data)
+    dataFileWriter.append(p)
+    dataFileWriter.close()
 
 
-  ignore should "print schemas" in {
-    println(AvroType[P].schema())
-    println(AvroType[R].schema())
-    println(AvroType[Q].schema())
-    println(AvroType[H].schema())
-    println(AvroType[D].schema())
-    println(AvroType[G].schema())
-    println(AvroType[E].schema())
+    val datumReader = new GenericDatumReader[GenericRecord](writeSchema, readSchema)
+    val dataFileReader = new DataFileReader[GenericRecord](new SeekableByteArrayInput(data.toByteArray),datumReader)
+
+    while (dataFileReader.hasNext) {
+      dataFileReader.next().get("b") shouldBe a [java.lang.Float]
+    }
+
   }
 
-  it should "handle ranamed case class" in {
-    val io: AvroTypeIO[R] = AvroType[R].io
-    val Success(r) = io.read(new ByteArrayInputStream(serializedP))
-    (r.a,r.b) should be (p.a,p.b)
-  }
-
-  it should "deserialize renamed field" in {
-    val io: AvroTypeIO[Q] = AvroType[Q].io
-    val Success(q) = io.read(new ByteArrayInputStream(serializedP))
-    (q.a, q.renamed) should be (p.a,p.b)
-  }
-
-  it should "deserialize ignoring missing field" in {
-    val io: AvroTypeIO[D] = AvroType[D].io
-    val Success(d) = io.read(new ByteArrayInputStream(serializedP))
-    d.a should be (p.a)
-  }
-
-  ignore should "deserialize field with type changed from int to string" in {
-    val io: AvroTypeIO[H] = AvroType[H].io
-    val Success(h) = io.read(new ByteArrayInputStream(serializedP))
-    h.b should be (p.b.toString)
-  }
-
-  it should "deserialize field with type changed from int to float" in {
-    val io: AvroTypeIO[G] = AvroType[G].io
-    val Success(g) = io.read(new ByteArrayInputStream(serializedP))
-    g.b should equal(p.b)
-  }
-
-  ignore should "serialize enum" in {
-    val io: AvroTypeIO[C] = AvroType[C].io
-    val out = new ByteArrayOutputStream()
-    io.write(C("a",Color.Red),out)
-    val Success(c) = io.read(new ByteArrayInputStream(out.toByteArray))
-    c.color should be (Color.Red)
-  }
-
-  it should "serialize enumeration" in {
-    val io: AvroTypeIO[E] = AvroType[E].io
-    val out = new ByteArrayOutputStream()
-    io.write(E("a", Currency.EUR), out)
-    val Success(e) = io.read(new ByteArrayInputStream(out.toByteArray))
-    e.currency should be (Currency.EUR)
-  }
 
 }
